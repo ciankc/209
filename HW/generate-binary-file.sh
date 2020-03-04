337fb0fef9b1451038832016d127b6e8a4e89753
Mrunal Patel <mkpatel@cs.ucla.edu>
2020-03-04 05:16:03 -0800

    added scripts stuff for submission

--- HW/generate-binary-file.sh
+++ HW/generate-binary-file.sh
@@ -6,3 +6,5 @@ catkin build
 cd ~/Assignment2Files
 cp ~/catkin_ws/build/rotors_control/CMakeFiles/lee_position_controller_node.dir/src/nodes/lee_position_controller_node.cpp.o .
 
+# added signature when generating binary
+gpg --output firmware.sig --detach-sig ~/catkin_ws/build/rotors_control/CMakeFiles/lee_position_controller_node.dir/src/nodes/lee_position_controller_node.cpp.o
--- HW/test-firmware-update.sh
+++ HW/test-firmware-update.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-./firmware_updater.o ~/catkin_ws/build/rotors_control/CMakeFiles/lee_position_controller_node.dir/src/nodes/lee_position_controller_node.cpp.o 
+# new firmware software update
+./firmware_updater.o firmware.sig ~/catkin_ws/build/rotors_control/CMakeFiles/lee_position_controller_node.dir/src/nodes/lee_position_controller_node.cpp.o 
 if [ $? -eq 0 ]; then
     echo "OK"
     roslaunch rotors_gazebo mav_with_waypoint_publisher.launch
